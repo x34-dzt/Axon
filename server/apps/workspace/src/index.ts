8be52168e771af678b3f20364140b8634aaca98e
x34-dzt <shruvparmar@gmail.com>
2026-03-09 16:42:07 +0530

    feat: added workspace service running on port 3003

---
+++ server/apps/workspace/src/index.ts
@@ -0,0 +1,24 @@
+import { Hono } from "hono";
+import { cors } from "hono/cors";
+
+const app = new Hono();
+
+app.use(cors());
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/health", (c) => {
+  return c.json(
+    {
+      message: "workspace service is healthy",
+    },
+    { status: 200 },
+  );
+});
+
+export default {
+  port: 3003,
+  fetch: app.fetch,
+};
