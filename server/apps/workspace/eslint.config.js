550a9b6e41413ea44c1f4dd4c328cc579a93b4be
x34-dzt <shruvparmar@gmail.com>
2026-03-09 21:53:11 +0530

    refactor: rename monorepo from axon to tsukuyomi

--- server/apps/workspace/eslint.config.js
+++ server/apps/workspace/eslint.config.js
@@ -1,4 +1,4 @@
-import baseConfig from "@axon/eslint-config/base";
+import baseConfig from "@tsukuyomi/eslint-config/base";
 
 /** @type {import("typescript-eslint").Config} */
 export default [{ ignores: ["dist"] }, ...baseConfig];
