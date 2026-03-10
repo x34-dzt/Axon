3da9cb6e22a624adff739a6d4d3cb283e9b0d7be
x34-dzt <shruvparmar@gmail.com>
2026-03-10 23:40:52 +0530

    chore: update dependencies for elysia migration

--- server/packages/db/src/create-db.ts
+++
@@ -1,7 +0,0 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-
-export function createDb(connectionString: string) {
-  return drizzle(connectionString);
-}
-
-export type Db = ReturnType<typeof createDb>;
--- server/packages/db/src/index.ts
+++ server/packages/db/src/index.ts
@@ -1,5 +1,13 @@
-export { baseColumns } from "./base-columns";
-export { createId, type } from "./id";
-export { createDb } from "./factory";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-export type { Db } from "./factory.ts";
+const pool = new Pool({
+  host: "localhost",
+  port: 5432,
+  user: "postgres",
+  password: "",
+  database: "tsukuyomi",
+});
+
+const db = drizzle(pool, { casing: "snake_case" });
+export { db };
