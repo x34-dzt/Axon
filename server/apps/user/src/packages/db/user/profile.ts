3ba2cecf567a80b97134285be10f8666d576f51a
x34-dzt <shruvparmar@gmail.com>
2026-03-10 23:40:24 +0530

    chore: remove user microservice

--- server/apps/user/src/packages/db/user/profile.ts
+++
@@ -1,7 +0,0 @@
-import { createInsertSchema, createSelectSchema } from "drizzle-typebox";
-import { profileTable } from "./profile.sql";
-
-export const profileSelectSchema = createSelectSchema(profileTable);
-export const profileInsertSchema = createInsertSchema(profileTable);
-
-export class Profile {}
--- server/apps/user/src/packages/db/user/user.ts
+++
@@ -1,8 +0,0 @@
-export { userTable } from "./user.sql";
-import { createSelectSchema, createInsertSchema } from "drizzle-typebox";
-import { userTable } from "./user.sql";
-
-export const userSelectScheam = createSelectSchema(userTable);
-export const userInsertScheam = createInsertSchema(userTable);
-
-export class User {}
