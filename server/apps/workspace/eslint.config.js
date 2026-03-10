88612cef77c250c1f19d2c025ed2d21074873df3
x34-dzt <shruvparmar@gmail.com>
2026-03-10 23:40:39 +0530

    chore: remove workspace microservice

--- server/apps/workspace/eslint.config.js
+++
@@ -1,4 +0,0 @@
-import baseConfig from "@tsukuyomi/eslint-config/base";
-
-/** @type {import("typescript-eslint").Config} */
-export default [{ ignores: ["dist"] }, ...baseConfig];
--- server/apps/workspace/src/packages/db/workspace/workspace.ts
+++
@@ -1,7 +0,0 @@
-import { createInsertSchema, createSelectSchema } from "drizzle-typebox";
-import { workspaceTable } from "./workspace.sql";
-
-export const workspaceSelectScheam = createSelectSchema(workspaceTable);
-export const workspaceInsertScheam = createInsertSchema(workspaceTable);
-
-export class Workspace {}
