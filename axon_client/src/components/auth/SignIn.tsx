78dccefd958fee63e536d20da46b17c40b57e2b4
x34-dzt <shruvparmar@gmail.com>
2026-03-14 01:09:28 +0530

    refactor(frontend): refactor auth components

--- axon_client/src/components/auth/SignIn.tsx
+++ axon_client/src/components/auth/SignIn.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import React, { type FormEvent, useEffect, useState } from "react";
+import { type FormEvent, useEffect, useState } from "react";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import Link from "next/link";
 import { Input } from "../ui/input";
@@ -17,83 +17,82 @@ import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/stores/auth";
 
 const SignInCard = () => {
-	const { isLoading, error, message, login, logout } = useAuthStore();
+  const { isLoading, error, message, login, logout } = useAuthStore();
 
-	// User sign in data
-	const [email, setEmail] = useState<string>("");
-	const [password, setPassword] = useState<string>("");
-	const router = useRouter();
-	const [signInMessage, setSignInMessage] = useState<string | null>("");
-	// handling submission of data
-	const handleSubmitData = async (e: FormEvent) => {
-		e.preventDefault();
-		const authenticated = await login(email, password);
-		if (authenticated) {
-			router.push("/");
-		}
-	};
+  // User sign in data
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const [signInMessage, setSignInMessage] = useState<string | null>("");
+  // handling submission of data
+  const handleSubmitData = async (e: FormEvent) => {
+    e.preventDefault();
+    const authenticated = await login(email, password);
+    if (authenticated) {
+      router.push("/");
+    }
+  };
 
-	const handleSignInMessage = () => {
-		setSignInMessage(message);
-		setTimeout(() => {
-			setSignInMessage("");
-		}, 1000);
-	};
+  const handleSignInMessage = () => {
+    setSignInMessage(message);
+    setTimeout(() => {
+      setSignInMessage("");
+    }, 1000);
+  };
 
-	// biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
-	useEffect(() => {
-		handleSignInMessage();
-	}, [message]);
+  // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
+  useEffect(() => {
+    handleSignInMessage();
+  }, [message]);
 
-	return (
-		<Card className="px-[20px] border-neutral-700 text-white bg-customMain rounded-[12px] py-[40px] w-[450px]">
-			<CardHeader>
-				<CardTitle className="text-center text-[20px]">
-					Sign in to axon
-				</CardTitle>
-				<CardDescription className="text-center">
-					Don&apos;t have an account? &nbsp;
-					<Link href="/auth/sign-up" className="hover:underline">
-						Sign up
-					</Link>
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<form
-					onSubmit={handleSubmitData}
-					className="w-full gap-[15px] flex flex-col"
-				>
-					<Input
-						className="bg-neutral-900 text-[15px] border-neutral-700 rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
-						type="email"
-						disabled={isLoading}
-						placeholder="Enter your email"
-						required
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<Input
-						className="bg-neutral-900 text-[15px] border-neutral-700 rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
-						type="password"
-						disabled={isLoading}
-						placeholder="Enter your password"
-						required
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<Button type="submit" variant="axon" disabled={false}>
-						{isLoading ? "Signing in.." : "Sign in"}
-					</Button>
-					{error && <p className="text-red-500 text-center">{error}</p>}
-					{signInMessage && (
-						<p className="text-green-500 text-center">{signInMessage}</p>
-					)}
-				</form>
-			</CardContent>
-			<CardContent>
-				<Separator className="bg-neutral-700" />
-			</CardContent>
-			<CardFooter>
-				Kindly remember your password meow :3
-				{/* <Button
+  return (
+    <Card className="px-[20px] border-neutral-700 text-white bg-customMain rounded-[12px] py-[40px] w-[450px]">
+      <CardHeader>
+        <CardTitle className="text-center text-[20px]">
+          Sign in to axon
+        </CardTitle>
+        <CardDescription className="text-center">
+          Don&apos;t have an account? &nbsp;
+          <Link href="/auth/sign-up" className="hover:underline">
+            Sign up
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form
+          onSubmit={handleSubmitData}
+          className="w-full gap-[15px] flex flex-col"
+        >
+          <Input
+            className="bg-neutral-900 text-[15px] border-neutral-700 rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
+            type="email"
+            disabled={isLoading}
+            placeholder="Enter your email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            className="bg-neutral-900 text-[15px] border-neutral-700 rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
+            type="password"
+            disabled={isLoading}
+            placeholder="Enter your password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" variant="axon" disabled={false}>
+            {isLoading ? "Signing in.." : "Sign in"}
+          </Button>
+          {error && <p className="text-red-500 text-center">{error}</p>}
+          {signInMessage && (
+            <p className="text-green-500 text-center">{signInMessage}</p>
+          )}
+        </form>
+      </CardContent>
+      <CardContent>
+        <Separator className="bg-neutral-700" />
+      </CardContent>
+      <CardFooter>
+        {/* <Button
 					type="button"
 					className="w-full relative"
 					variant="axon"
@@ -104,9 +103,9 @@ const SignInCard = () => {
 					</div>
 					Sign in with Google
 				</Button> */}
-			</CardFooter>
-		</Card>
-	);
+      </CardFooter>
+    </Card>
+  );
 };
 
 export default SignInCard;
--- axon_client/src/components/auth/SignUp.tsx
+++ axon_client/src/components/auth/SignUp.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { type FormEvent, useEffect, useState } from "react";
+import { type FormEvent, useState } from "react";
 import {
-	Card,
-	CardContent,
-	CardDescription,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
-import { FcGoogle } from "react-icons/fc";
 import Link from "next/link";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
@@ -18,79 +17,79 @@ import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/stores/auth";
 
 const SignUpCard = () => {
-	const router = useRouter();
-	const [localUsername, setLocalUsername] = useState<string>("");
-	const [email, setEmail] = useState<string>("");
-	const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const [localUsername, setLocalUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
 
-	const { isLoading, error, signUp, message } = useAuthStore();
+  const { isLoading, error, signUp, message } = useAuthStore();
 
-	const sendData = async (e: FormEvent) => {
-		e.preventDefault();
-		//using the custom hook for
-		const authenticated = await signUp(email, localUsername, password);
-		if (authenticated) {
-			router.push("/");
-		}
-	};
+  const sendData = async (e: FormEvent) => {
+    e.preventDefault();
+    //using the custom hook for
+    const authenticated = await signUp(email, localUsername, password);
+    if (authenticated) {
+      router.push("/");
+    }
+  };
 
-	return (
-		<Card className="px-[20px] text-white border-neutral-700 bg-customMain rounded-[12px] py-[40px] w-[450px]">
-			<CardHeader>
-				<CardTitle className="text-center text-[20px]">
-					Sign up to axon
-				</CardTitle>
-				<CardDescription className="text-center">
-					Already have an account? &nbsp;
-					<Link href="/auth/sign-in" className=" hover:underline">
-						Sign in
-					</Link>
-				</CardDescription>
-			</CardHeader>
-			<CardContent>
-				<form onSubmit={sendData} className="w-full gap-[15px] flex flex-col ">
-					<Input
-						className=" bg-neutral-900 border-neutral-700 text-[15px] rounded-[8px]  focus-visible:ring-0 focus-visible:ring-transparent"
-						type="email"
-						disabled={isLoading}
-						value={email}
-						placeholder="Enter your email"
-						required
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-					<Input
-						className="  bg-neutral-900 border-neutral-700 text-[15px] rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
-						type="text"
-						disabled={isLoading}
-						value={localUsername}
-						placeholder="Enter your username"
-						required
-						onChange={(e) => setLocalUsername(e.target.value)}
-					/>
-					<Input
-						className=" bg-neutral-900 border-neutral-700 text-[15px]  rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
-						type="password"
-						disabled={isLoading}
-						value={password}
-						placeholder="Enter your password"
-						required
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-					<Button type="submit" variant="axon" disabled={isLoading}>
-						{isLoading ? "Submitting" : "Submit"}
-					</Button>
-				</form>
-				{error && <p className="text-red-500 text-center">{error}</p>}
-				{message !== null && (
-					<p className="text-green-500 text-center">Signed up successfully</p>
-				)}
-			</CardContent>
-			<CardContent>
-				<Separator className="bg-neutral-700" />
-			</CardContent>
-			<CardFooter>Kindly remember your password meow :3</CardFooter>
-		</Card>
-	);
+  return (
+    <Card className="px-[20px] text-white border-neutral-700 bg-customMain rounded-[12px] py-[40px] w-[450px]">
+      <CardHeader>
+        <CardTitle className="text-center text-[20px]">
+          Sign up to axon
+        </CardTitle>
+        <CardDescription className="text-center">
+          Already have an account? &nbsp;
+          <Link href="/auth/sign-in" className=" hover:underline">
+            Sign in
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={sendData} className="w-full gap-[15px] flex flex-col ">
+          <Input
+            className=" bg-neutral-900 border-neutral-700 text-[15px] rounded-[8px]  focus-visible:ring-0 focus-visible:ring-transparent"
+            type="email"
+            disabled={isLoading}
+            value={email}
+            placeholder="Enter your email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            className="  bg-neutral-900 border-neutral-700 text-[15px] rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
+            type="text"
+            disabled={isLoading}
+            value={localUsername}
+            placeholder="Enter your username"
+            required
+            onChange={(e) => setLocalUsername(e.target.value)}
+          />
+          <Input
+            className=" bg-neutral-900 border-neutral-700 text-[15px]  rounded-[8px] focus-visible:ring-0 focus-visible:ring-transparent"
+            type="password"
+            disabled={isLoading}
+            value={password}
+            placeholder="Enter your password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Button type="submit" variant="axon" disabled={isLoading}>
+            {isLoading ? "Submitting" : "Submit"}
+          </Button>
+        </form>
+        {error && <p className="text-red-500 text-center">{error}</p>}
+        {message !== null && (
+          <p className="text-green-500 text-center">Signed up successfully</p>
+        )}
+      </CardContent>
+      <CardContent>
+        <Separator className="bg-neutral-700" />
+      </CardContent>
+      <CardFooter>Kindly remember your password meow :3</CardFooter>
+    </Card>
+  );
 };
 
 export default SignUpCard;
